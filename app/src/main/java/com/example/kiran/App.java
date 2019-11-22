faa70a5b64851605a1cdcf36fdc2506c8cb15833
jagadevi <jagadevi@gmail.com>
2019-11-22 22:37:41 +0530

    updated

--- app/src/main/java/com/example/kiran/App.java
+++ app/src/main/java/com/example/kiran/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Kiran kumar!" );
+        System.out.println( "Hello Kiran kumar uppin!" );
     }
 }
